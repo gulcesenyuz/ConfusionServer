0480581c4331a078878878adc5b8095439e27bec
gülce <gizemgulce@gmail.com>
2021-04-12 11:24:12 +0300

    pıpulation for users

--- models/user.js
+++ models/user.js
@@ -6,6 +6,14 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 
 var User = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
@@ -15,4 +23,3 @@ var User = new Schema({
 User.plugin(passportLocalMongoose);
 
 var User = mongoose.model('User', User);
-module.exports = User;
