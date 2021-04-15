3766ab0e0416a1ba2ec2bd4d74a18b37ba57640e
gülce <gizemgulce@gmail.com>
2021-04-15 21:37:48 +0300

    file upload

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var config = require('./config');
+var uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
+
 
 var passport = require('passport');
 var authenticate = require('./authenticate');
@@ -81,6 +84,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
